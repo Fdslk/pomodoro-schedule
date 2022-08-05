5da2fce24be810a02bdf69d023874a6d0af940e7
zengqiang.fang <zengqiang.fang@thoughtworks.com>
2022-08-05 09:41:55 +0800

    初始化 intellig plugin项目

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pomodprp-schedule
 Intellig plugin for time management
+
+## some concept
+* plugins.xml
+* Action
+* Dialog
---
+++ src/main/kotlin/com/zqf/pomodoroschedule/MainAction.kt
@@ -0,0 +1,10 @@
+package com.zqf.pomodoroschedule
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class MainAction: AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        TODO("Not yet implemented")
+    }
+}
